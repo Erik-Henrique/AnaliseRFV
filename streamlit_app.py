58c5682f267a2fbae04c9e385a554cf444766807
Erik Henrique Dias Ferreira <150151321+Erik-Henrique@users.noreply.github.com>
2025-01-08 16:41:57 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,84 +71,80 @@ def main():
     image = Image.open("Bank-Branding.jpg")
     st.sidebar.image(image)
 
-    # Botão para carregar arquivo na aplicação
-    st.sidebar.write("## Suba o arquivo")
-    data_file_1 = st.sidebar.file_uploader("Bank marketing data", type = ['csv','xlsx'])
 
-    # Verifica se há conteúdo carregado na aplicação
-    if (data_file_1 is not None):
-        df_compras = pd.read_csv(data_file_1, infer_datetime_format=True, parse_dates=['DiaCompra'])
+df_compras = pd.read_csv("https://raw.githubusercontent.com/Erik-Henrique/AnaliseRFV/refs/heads/main/dados_test_input%202.csv")
 
-        st.write('## Recência (R)')
+st.write('## Recência (R)')
 
         
-        dia_atual = df_compras['DiaCompra'].max()
-        st.write('Dia máximo na base de dados: ', dia_atual)
+dia_atual = df_compras['DiaCompra'].max()
+st.write('Dia máximo na base de dados: ', dia_atual)
 
-        st.write('Quantos dias faz que o cliente fez a sua última compra?')
+st.write('Quantos dias faz que o cliente fez a sua última compra?')
 
-        df_recencia = df_compras.groupby(by='ID_cliente', as_index=False)['DiaCompra'].max()
-        df_recencia.columns = ['ID_cliente','DiaUltimaCompra']
-        df_recencia['Recencia'] = df_recencia['DiaUltimaCompra'].apply(lambda x: (dia_atual - x).days)
-        st.write(df_recencia.head())
+df_recencia = df_compras.groupby(by='ID_cliente', as_index=False)['DiaCompra'].max()
+df_recencia.columns = ['ID_cliente','DiaUltimaCompra']
+df_recencia['Recencia'] = df_recencia['DiaUltimaCompra'].apply(lambda x: (dia_atual - x).days)
+st.write(df_recencia.head())
 
-        df_recencia.drop('DiaUltimaCompra', axis=1, inplace=True)
+df_recencia.drop('DiaUltimaCompra', axis=1, inplace=True)
 
-        st.write('## Frequência (F)')
-        st.write('Quantas vezes cada cliente comprou com a gente?')
-        df_frequencia = df_compras[['ID_cliente','CodigoCompra']].groupby('ID_cliente').count().reset_index()
-        df_frequencia.columns = ['ID_cliente','Frequencia']
-        st.write(df_frequencia.head())
+st.write('## Frequência (F)')
+st.write('Quantas vezes cada cliente comprou com a gente?')
+df_frequencia = df_compras[['ID_cliente','CodigoCompra']].groupby('ID_cliente').count().reset_index()
+df_frequencia.columns = ['ID_cliente','Frequencia']
+st.write(df_frequencia.head())
 
-        st.write('## Valor (V)')
-        st.write('Quanto que cada cliente gastou no periodo?')
-        df_valor = df_compras[['ID_cliente','ValorTotal']].groupby('ID_cliente').sum().reset_index()
-        df_valor.columns = ['ID_cliente','Valor']
-        st.write(df_valor.head())
+st.write('## Valor (V)')
+st.write('Quanto que cada cliente gastou no periodo?')
+df_valor = df_compras[['ID_cliente','ValorTotal']].groupby('ID_cliente').sum().reset_index()
+df_valor.columns = ['ID_cliente','Valor']
+st.write(df_valor.head())
         
 
-        st.write('## Tabela RFV final')
-        df_RF = df_recencia.merge(df_frequencia, on='ID_cliente')
-        df_RFV = df_RF.merge(df_valor, on='ID_cliente')
-        df_RFV.set_index('ID_cliente', inplace=True)
-        st.write(df_RFV.head())
+st.write('## Tabela RFV final')
+df_RF = df_recencia.merge(df_frequencia, on='ID_cliente')
+df_RFV = df_RF.merge(df_valor, on='ID_cliente')
+df_RFV.set_index('ID_cliente', inplace=True)
+st.write(df_RFV.head())
 
-        st.write('## Segmentação utilizando o RFV')
-        st.write("Um jeito de segmentar os clientes é criando quartis para cada componente do RFV, sendo que o melhor quartil é chamado de 'A', o segundo melhor quartil de 'B', o terceiro melhor de 'C' e o pior de 'D'. O melhor e o pior depende da componente. Po exemplo, quanto menor a recência melhor é o cliente (pois ele comprou com a gente tem pouco tempo) logo o menor quartil seria classificado como 'A', já pra componente frêquencia a lógica se inverte, ou seja, quanto maior a frêquencia do cliente comprar com a gente, melhor ele/a é, logo, o maior quartil recebe a letra 'A'.")
-        st.write('Se a gente tiver interessado em mais ou menos classes, basta a gente aumentar ou diminuir o número de quantils pra cada componente.')
+st.write('## Segmentação utilizando o RFV')
+st.write("Um jeito de segmentar os clientes é criando quartis para cada componente do RFV, sendo que o melhor quartil é chamado de 'A', o segundo melhor quartil de 'B', o terceiro melhor de 'C' e o pior de 'D'. O melhor e o pior depende da componente. Po exemplo, quanto menor a recência melhor é o cliente (pois ele comprou com a gente tem pouco tempo) logo o menor quartil seria classificado como 'A', já pra componente frêquencia a lógica se inverte, ou seja, quanto maior a frêquencia do cliente comprar com a gente, melhor ele/a é, logo, o maior quartil recebe a letra 'A'.")
+st.write('Se a gente tiver interessado em mais ou menos classes, basta a gente aumentar ou diminuir o número de quantils pra cada componente.')
 
-        st.write('Quartis para o RFV')
-        quartis = df_RFV.quantile(q=[0.25,0.5,0.75])
-        st.write(quartis)
+st.write('Quartis para o RFV')
+quartis = df_RFV.quantile(q=[0.25,0.5,0.75])
+st.write(quartis)
 
-        st.write('Tabela após a criação dos grupos')
-        df_RFV['R_quartil'] = df_RFV['Recencia'].apply(recencia_class,
+st.write('Tabela após a criação dos grupos')
+df_RFV['R_quartil'] = df_RFV['Recencia'].apply(recencia_class,
                                                         args=('Recencia', quartis))
-        df_RFV['F_quartil'] = df_RFV['Frequencia'].apply(freq_val_class,
+df_RFV['F_quartil'] = df_RFV['Frequencia'].apply(freq_val_class,
                                                         args=('Frequencia', quartis))
-        df_RFV['V_quartil'] = df_RFV['Valor'].apply(freq_val_class,
+df_RFV['V_quartil'] = df_RFV['Valor'].apply(freq_val_class,
                                                     args=('Valor', quartis))
-        df_RFV['RFV_Score'] = (df_RFV.R_quartil 
+df_RFV['RFV_Score'] = (df_RFV.R_quartil 
                             + df_RFV.F_quartil 
                             + df_RFV.V_quartil)
-        st.write(df_RFV.head())
+st.write(df_RFV.head())
 
-        st.write('Quantidade de clientes por grupos')
-        st.write(df_RFV['RFV_Score'].value_counts())
+st.write('Quantidade de clientes por grupos')
+st.write(df_RFV['RFV_Score'].value_counts())
 
-        st.write('#### Clientes com menor recência, maior frequência e maior valor gasto')
-        st.write(df_RFV[df_RFV['RFV_Score']=='AAA'].sort_values('Valor', ascending=False).head(10))
+st.write('#### Clientes com menor recência, maior frequência e maior valor gasto')
+st.write(df_RFV[df_RFV['RFV_Score']=='AAA'].sort_values('Valor', ascending=False).head(10))
 
-        st.write('### Ações de marketing/CRM')
+st.write('### Ações de marketing/CRM')
 
-        dict_acoes = {'AAA': 'Enviar cupons de desconto, Pedir para indicar nosso produto pra algum amigo, Ao lançar um novo produto enviar amostras grátis pra esses.',
-        'DDD': 'Churn! clientes que gastaram bem pouco e fizeram poucas compras, fazer nada',
-        'DAA': 'Churn! clientes que gastaram bastante e fizeram muitas compras, enviar cupons de desconto para tentar recuperar',
-        'CAA': 'Churn! clientes que gastaram bastante e fizeram muitas compras, enviar cupons de desconto para tentar recuperar'
-        }
+dict_acoes = {'AAA': 'Enviar cupons de desconto, Pedir para indicar nosso produto pra algum amigo, Ao lançar um novo produto enviar amostras grátis pra esses.',
+'DDD': 'Churn! clientes que gastaram bem pouco e fizeram poucas compras, fazer nada',
+'DAA': 'Churn! clientes que gastaram bastante e fizeram muitas compras, enviar cupons de desconto para tentar recuperar',
+'CAA': 'Churn! clientes que gastaram bastante e fizeram muitas compras, enviar cupons de desconto para tentar recuperar'
+}
 
-        df_RFV['acoes de marketing/crm'] = df_RFV['RFV_Score'].map(dict_acoes)
-        st.write(df_RFV.head())
+df_RFV['acoes de marketing/crm'] = df_RFV['RFV_Score'].map(dict_acoes)
+st.write(df_RFV.head())
+
 if __name__ == '__main__':
 	main()
     
